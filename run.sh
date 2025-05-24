0a19bec56134c97fd12e836d699abd0e1ced4f38
Shreeshail G <shreeshailgumgeri@gmail.com>
2025-05-24 17:04:06 +0000

    Cleaning up un necessary files

--- run.sh
+++
@@ -1,6 +0,0 @@
-# Replace with the latest version if needed
-PACKER_VERSION="1.11.2"
-wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
-unzip packer_${PACKER_VERSION}_linux_amd64.zip
-sudo mv packer /usr/local/bin/
-packer version
